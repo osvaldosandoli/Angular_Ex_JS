5f634e3728ab019d3dc86704da45aecb42c409e2
Osvaldo José Sandoli Neto <osvaldosandoliPC@gmail.com>
2024-05-08 22:23:43 -0300

    commit two way

--- 08_05/src/app/app.component.ts
+++ 08_05/src/app/app.component.ts
@@ -1,13 +1,16 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { RouterOutlet } from "@angular/router";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = '08_05';
+    title = 'introducao-angular';
+
+    nome: String = "Teste";
 }
